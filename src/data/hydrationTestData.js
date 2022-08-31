1ab23c0b83853cde902ad395341cca1c51bd568a
Nicole Forseth <forseth.nico@gmail.com>
2022-08-31 16:53:37 -0600

    Add hydration data test file

---
+++ src/data/hydrationTestData.js
@@ -0,0 +1,54 @@
+hydrationData = [
+  {
+    userID: 1,
+    date: "2019/06/15",
+    numOunces: 37,
+  },
+  {
+    userID: 1,
+    date: "2019/06/16",
+    numOunces: 69,
+  },
+  {
+    userID: 38,
+    date: "2019/06/16",
+    numOunces: 54,
+  },
+  {
+    userID: 1,
+    date: "2019/06/17",
+    numOunces: 96,
+  },
+  {
+    userID: 27,
+    date: "2019/06/17",
+    numOunces: 99,
+  },
+  {
+    userID: 1,
+    date: "2019/06/18",
+    numOunces: 61,
+  },
+  {
+    userID: 1,
+    date: "2019/06/19",
+    numOunces: 91,
+  },
+  {
+    userID: 1,
+    date: "2019/06/20",
+    numOunces: 50,
+  },
+  {
+    userID: 1,
+    date: "2019/06/21",
+    numOunces: 50,
+  },
+  {
+    userID: 1,
+    date: "2019/06/22",
+    numOunces: 43,
+  },
+];
+
+module.exports = hydrationData;
